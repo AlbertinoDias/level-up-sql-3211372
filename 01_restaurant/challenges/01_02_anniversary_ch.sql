e6e119188e2eb40c35de21debf5010ed65167531
Francisco Borges <36789123+AlbertinoDias@users.noreply.github.com>
2025-01-07 17:56:20 +0000

    restaurant 1 and 2

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -1,6 +1,9 @@
 -- Create a table in the database to store customer
 -- responses to our anniversary invitation.
-
+Create table AnniversaryAttendees(
+CustomerID INTEGER,
+PartySize INTEGER
+)
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
 
