337778ce7da148243ed343869264e07d1bb01d5d
Francisco Borges <36789123+AlbertinoDias@users.noreply.github.com>
2025-01-08 17:51:32 +0000

    01_03 - 01_07

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,14 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+select Type, Name, Price, description from dishes
+order by price asc
+
+select Type, Name, Price, description  from dishes
+where Type = 'Beverage' or Type = 'Appetizer'
+order by price asc
+
+select Type, Name, Price, description  from dishes
+where Type != 'Beverage'
+order by price asc
--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,9 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+
+UPDATE customers
+SET Address = '74 Pine St.', city = 'New York', state = 'NY'
+where FirstName = 'Taylor' and LastName ='Jenkins' and address = '27170 6th Ave.'; 
+
+select * from customers where firstname = 'Taylor'
