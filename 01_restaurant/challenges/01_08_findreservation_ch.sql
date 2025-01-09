3bbccae9f67d26f6f2146d05117b32e525abba81
Francisco Borges <36789123+AlbertinoDias@users.noreply.github.com>
2025-01-09 17:58:25 +0000

    01_08

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,12 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+select customers.firstname, customers.lastname,
+ reservations.date, reservations.partysize from reservations
+left join customers 
+on customers.customerid= reservations.customerid
+where reservations.customerid in (select customerid from customers 
+where lastname like 'Ste%')
+and partysize = 4
+order by 3 desc
+
